5cba816f1e8a125d5fca9e7bd0ad9fa2bf115b32
bsekisser <squirmyworms@embarqmail.com>
2022-08-08 05:27:00 -0400

    add and use 'instruction register'...  part 2, thumb.
    	modified:   source/csx_core_thumb.c

--- source/csx_core_thumb.c
+++ source/csx_core_thumb.c
@@ -15,21 +15,21 @@ static void csx_core_thumb_disasm(csx_core_p core, uint32_t address, uint32_t op
 
 /* **** */
 
-static void csx_core_thumb_add_sub_rn_rd(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_add_sub_rn_rd(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 
-	const int bit_i = BEXT(opcode, 10);
-	const uint8_t op2 = BEXT(opcode, 9);
+	const int bit_i = BEXT(IR, 10);
+	const uint8_t op2 = BEXT(IR, 9);
 
-	const csx_reg_t rm = mlBFEXT(opcode, 8, 6);
+	const csx_reg_t rm = mlBFEXT(IR, 8, 6);
 	uint32_t rm_v = rm;
 	if(!bit_i)
 		rm_v = csx_reg_get(core, rm);
 	
-	const csx_reg_t rn = mlBFEXT(opcode, 5, 3);
+	const csx_reg_t rn = mlBFEXT(IR, 5, 3);
 	const uint32_t rn_v = csx_reg_get(core, rn);
-	const csx_reg_t rd = mlBFEXT(opcode, 2, 0);
+	const csx_reg_t rd = mlBFEXT(IR, 2, 0);
 	
 	uint32_t rd_v = rn_v;
 	
@@ -70,12 +70,12 @@ static void csx_core_thumb_add_sub_rn_rd(csx_core_p core, uint16_t opcode)
 		csx_core_flags_nzcv_add(core, rd_v, rn_v, rm_v);
 }
 
-static void csx_core_thumb_add_sub_sp_i7(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_add_sub_sp_i7(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 
-	const int sub = BEXT(opcode, 7);
-	const uint16_t imm7 = mlBFMOV(opcode, 6, 0, 2);
+	const int sub = BEXT(IR, 7);
+	const uint16_t imm7 = mlBFMOV(IR, 6, 0, 2);
 
 	const uint32_t sp_v = SP;
 	uint32_t res = sp_v;
@@ -96,13 +96,13 @@ static void csx_core_thumb_add_sub_sp_i7(csx_core_p core, uint16_t opcode)
 	SP = res;
 }
 
-static void csx_core_thumb_add_rd_pcsp_i(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_add_rd_pcsp_i(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 	
-	const int pcsp = BEXT(opcode, 11);
-	const csx_reg_t rd = mlBFEXT(opcode, 10, 8);
-	const uint16_t imm8 = mlBFMOV(opcode, 7, 0, 2);
+	const int pcsp = BEXT(IR, 11);
+	const csx_reg_t rd = mlBFEXT(IR, 10, 8);
+	const uint16_t imm8 = mlBFMOV(IR, 7, 0, 2);
 	
 	uint32_t rd_v;
 	if(pcsp)
@@ -120,15 +120,15 @@ static void csx_core_thumb_add_rd_pcsp_i(csx_core_p core, uint16_t opcode)
 }
 
 
-static void csx_core_thumb_ascm_rd_i(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_ascm_rd_i(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 	int wb = 1;
 	
-	const uint8_t operation = mlBFEXT(opcode, 12, 11);
-	const csx_reg_t rd = mlBFEXT(opcode, 10, 8);
+	const uint8_t operation = mlBFEXT(IR, 12, 11);
+	const csx_reg_t rd = mlBFEXT(IR, 10, 8);
 	const uint32_t rd_v = csx_reg_get(core, rd);
-	const uint8_t imm8 = mlBFEXT(opcode, 7, 0);
+	const uint8_t imm8 = mlBFEXT(IR, 7, 0);
 	
 	uint32_t res = rd_v;
 	
@@ -156,7 +156,7 @@ static void csx_core_thumb_ascm_rd_i(csx_core_p core, uint16_t opcode)
 			break;
 		default:
 			LOG("operation = 0x%03x", operation);
-			csx_core_thumb_disasm(core, PC, opcode);
+			csx_core_thumb_disasm(core, IP, IR);
 			LOG_ACTION(exit(1));
 	}
 	
@@ -178,13 +178,11 @@ static void csx_core_thumb_ascm_rd_i(csx_core_p core, uint16_t opcode)
 	}
 }
 
-static void csx_core_thumb_bcc(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_bcc(csx_core_p core)
 {
-	IR = opcode;
-
-	const uint8_t cond = mlBFEXT(opcode, 11, 8);
+	const uint8_t cond = mlBFEXT(IR, 11, 8);
 	const uint8_t cce = csx_core_check_cc(core, cond);
-	const int32_t imm8 = mlBFEXTs(opcode, 7, 0) << 1;
+	const int32_t imm8 = mlBFEXTs(IR, 7, 0) << 1;
 
 	const uint32_t new_pc = PC_THUMB + imm8;
 
@@ -198,16 +196,16 @@ static void csx_core_thumb_bcc(csx_core_p core, uint16_t opcode)
 	}
 }
 
-static void csx_core_thumb_bx(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_bx(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 
-	const int tsbz = _check_sbz(opcode, 2, 0, 0, 0);
+	const int tsbz = _check_sbz(IR, 2, 0, 0, 0);
 	if(tsbz)
 		LOG_ACTION(exit(1));
 
-	const csx_reg_t rm = mlBFEXT(opcode, 6, 3);
-	const int link = BEXT(opcode, 7);
+	const csx_reg_t rm = mlBFEXT(IR, 6, 3);
+	const int link = BEXT(IR, 7);
 	
 	const uint32_t new_pc = csx_reg_get(core, rm);
 	const int thumb = new_pc & 1;
@@ -227,7 +225,7 @@ static void csx_core_thumb_bx(csx_core_p core, uint16_t opcode)
 	if(0) LOG("PC(0x%08x)", PC);
 }
 
-static void csx_core_thumb_bxx_b(csx_core_p core, int32_t eao, uint32_t opcode)
+static void csx_core_thumb_bxx_b(csx_core_p core, int32_t eao)
 {
 	CORE_T(const int cce = 1);
 
@@ -240,8 +238,7 @@ static void csx_core_thumb_bxx_b(csx_core_p core, int32_t eao, uint32_t opcode)
 
 static void csx_core_thumb_bxx_blx(
 	csx_core_p core,
-	int32_t eao,
-	uint32_t opcode)
+	int32_t eao)
 {
 	CORE_T(const int cce = 1);
 
@@ -251,7 +248,7 @@ static void csx_core_thumb_bxx_blx(
 
 	LR = PC | 1;
 
-	int blx = (0 == BEXT(opcode, 12));
+	int blx = (0 == BEXT(IR, 12));
 	
 	if(blx)
 		new_pc &= ~3;
@@ -267,13 +264,13 @@ static void csx_core_thumb_bxx_blx(
 	if(1) LOG("LR = 0x%08x, PC = 0x%08x", LR, PC);
 }
 
-static void csx_core_thumb_bxx(csx_core_p core, uint32_t opcode)
+static void csx_core_thumb_bxx(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 
 	for(int i = 0; i < 2; i++) {
-		int32_t eao = mlBFEXTs(opcode, 10, 0);
-		uint8_t h = mlBFEXT(opcode, 12, 11);
+		int32_t eao = mlBFEXTs(IR, 10, 0);
+		uint8_t h = mlBFEXT(IR, 12, 11);
 
 		if(0) CORE_TRACE("H = 0x%02x, LR = 0x%08x, PC = 0x%08x, EAO = 0x%08x",
 			h, LR, PC, eao);
@@ -281,19 +278,19 @@ static void csx_core_thumb_bxx(csx_core_p core, uint32_t opcode)
 		switch(h) {
 			case 0x00:
 				/* branch offset is from PC + 4, ie.. lr */
-				return(csx_core_thumb_bxx_b(core, eao << 1, opcode));
+				return(csx_core_thumb_bxx_b(core, eao << 1));
 			case 0x01:
 			case 0x03:
-				eao = mlBFMOV(opcode, 10, 0, 1);
-				return(csx_core_thumb_bxx_blx(core, eao, opcode));
+				eao = mlBFMOV(IR, 10, 0, 1);
+				return(csx_core_thumb_bxx_blx(core, eao));
 			case 0x02:
 				eao <<= 12;
 				LR = 2 + PC + eao;
 
-				opcode <<= 16;
-				opcode += csx_core_read(core, PC & ~1, sizeof(uint16_t));
+				IR <<= 16;
+				IR += csx_core_read(core, PC & ~1, sizeof(uint16_t));
 				
-				if((0x7 != mlBFEXT(opcode, 15, 13)) && !BEXT(opcode, 11)) {
+				if((0x7 != mlBFEXT(IR, 15, 13)) && !BEXT(IR, 11)) {
 					CORE_TRACE("/* xxx -- LR = 0x%08x + 0x%03x = 0x%08x */", PC, eao, LR);
 					return;
 				}
@@ -305,16 +302,16 @@ static void csx_core_thumb_bxx(csx_core_p core, uint32_t opcode)
 	}
 }
 
-static void csx_core_thumb_dp_rms_rdn(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_dp_rms_rdn(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 	
-	const uint8_t operation = mlBFEXT(opcode, 9, 6);
+	const uint8_t operation = mlBFEXT(IR, 9, 6);
 
-	const csx_reg_t rm = mlBFEXT(opcode, 5, 3);
+	const csx_reg_t rm = mlBFEXT(IR, 5, 3);
 	const uint32_t rm_v = csx_reg_get(core, rm);
 	
-	const csx_reg_t rd = mlBFEXT(opcode, 2, 0);
+	const csx_reg_t rd = mlBFEXT(IR, 2, 0);
 	const uint32_t rd_v = csx_reg_get(core, rd);
 	
 	uint32_t res = rd_v;
@@ -343,7 +340,7 @@ static void csx_core_thumb_dp_rms_rdn(csx_core_p core, uint16_t opcode)
 			break;
 		default:
 			LOG("operation = 0x%03x", operation);
-			csx_core_thumb_disasm(core, IP, opcode);
+			csx_core_thumb_disasm(core, IP, IR);
 			LOG_ACTION(exit(1));
 			break;
 	}
@@ -352,15 +349,15 @@ static void csx_core_thumb_dp_rms_rdn(csx_core_p core, uint16_t opcode)
 	csx_core_flags_nz(core, res);
 }
 
-static void csx_core_thumb_ldst_rd_i(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_ldst_rd_i(csx_core_p core)
 {
 //	core->ccs = "EA"; /* ??? */
 	CORE_T(const int cce = 1);
 	
-	const uint16_t operation = mlBFTST(opcode, 15, 12);
-	const int bit_l = BEXT(opcode, 11);
-	const csx_reg_t rd = mlBFEXT(opcode, 10, 8);
-	const uint16_t imm8 = mlBFMOV(opcode, 7, 0, 2);
+	const uint16_t operation = mlBFTST(IR, 15, 12);
+	const int bit_l = BEXT(IR, 11);
+	const csx_reg_t rd = mlBFEXT(IR, 10, 8);
+	const uint16_t imm8 = mlBFMOV(IR, 7, 0, 2);
 
 	uint8_t rn;
 	uint32_t ea;
@@ -376,7 +373,7 @@ static void csx_core_thumb_ldst_rd_i(csx_core_p core, uint16_t opcode)
 			break;
 		default:
 			LOG("operation = 0x%03x", operation);
-			csx_core_thumb_disasm(core, IP, opcode);
+			csx_core_thumb_disasm(core, IP, IR);
 			LOG_ACTION(exit(1));
 			break;
 	}
@@ -399,24 +396,24 @@ static void csx_core_thumb_ldst_rd_i(csx_core_p core, uint16_t opcode)
 		csx_core_write(core, ea, rd_v, sizeof(uint32_t));
 }
 
-static void csx_core_thumb_ldst_bwh_o_rn_rd(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_ldst_bwh_o_rn_rd(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 	
 //	struct {
-		const int bit_b = BEXT(opcode, 12);
-		const int bit_l = BEXT(opcode, 11);
+		const int bit_b = BEXT(IR, 12);
+		const int bit_l = BEXT(IR, 11);
 //	}bit;
 	
-	const uint8_t imm5 = mlBFEXT(opcode, 10, 6);
-	const csx_reg_t rn = mlBFEXT(opcode, 5, 3);
+	const uint8_t imm5 = mlBFEXT(IR, 10, 6);
+	const csx_reg_t rn = mlBFEXT(IR, 5, 3);
 	const uint32_t rn_v = csx_reg_get(core, rn);
-	const csx_reg_t rd = mlBFEXT(opcode, 2, 0);
+	const csx_reg_t rd = mlBFEXT(IR, 2, 0);
 	
 	const char *ss = "";
 	uint8_t size = 0;
 	
-	if(CSX_CORE_THUMB_LDST_BW_O_RN_RD == (opcode & CSX_CORE_THUMB_LDST_BW_O_RN_RD_MASK))
+	if(CSX_CORE_THUMB_LDST_BW_O_RN_RD == (IR & CSX_CORE_THUMB_LDST_BW_O_RN_RD_MASK))
 	{
 		if(bit_b)
 		{
@@ -454,22 +451,22 @@ static void csx_core_thumb_ldst_bwh_o_rn_rd(csx_core_p core, uint16_t opcode)
 		csx_core_write(core, ea, rd_v, size);
 }
 
-static void csx_core_thumb_ldst_rm_rn_rd(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_ldst_rm_rn_rd(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 	
 //	struct {
-		const int bit_l = BEXT(opcode, 11);
-		const uint8_t bwh = mlBFEXT(opcode, 10, 9);
+		const int bit_l = BEXT(IR, 11);
+		const uint8_t bwh = mlBFEXT(IR, 10, 9);
 //	}bit;
 	
-	const csx_reg_t rm = mlBFEXT(opcode, 8, 6);
+	const csx_reg_t rm = mlBFEXT(IR, 8, 6);
 	const uint32_t rm_v = csx_reg_get(core, rm);
 	
-	const csx_reg_t rn = mlBFEXT(opcode, 5, 3);
+	const csx_reg_t rn = mlBFEXT(IR, 5, 3);
 	const uint32_t rn_v = csx_reg_get(core, rn);
 	
-	const csx_reg_t rd = mlBFEXT(opcode, 2, 0);
+	const csx_reg_t rd = mlBFEXT(IR, 2, 0);
 	
 	const char *ss = "";
 	uint8_t size = 0;
@@ -510,18 +507,18 @@ static void csx_core_thumb_ldst_rm_rn_rd(csx_core_p core, uint16_t opcode)
 		csx_core_write(core, ea, rd_v, size);
 }
 
-static void csx_core_thumb_ldstm_rn_rxx(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_ldstm_rn_rxx(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 
 //	struct {
-		const int bit_l = BEXT(opcode, 11);
+		const int bit_l = BEXT(IR, 11);
 //	}bit;
 
-	const csx_reg_t rn = mlBFEXT(opcode, 10, 8);
+	const csx_reg_t rn = mlBFEXT(IR, 10, 8);
 	const uint32_t rn_v = csx_reg_get(core, rn);
 
-	const uint8_t rlist = mlBFEXT(opcode, 7, 0);
+	const uint8_t rlist = mlBFEXT(IR, 7, 0);
 	
 	const uint32_t start_address = rn_v;
 	const uint32_t end_address = start_address + (__builtin_popcount(rlist) << 2) - 4;
@@ -572,17 +569,17 @@ static void csx_core_thumb_ldstm_rn_rxx(csx_core_p core, uint16_t opcode)
 		wb ? "!" : "", reglist, rn_v);
 }
 
-static void csx_core_thumb_sbi_imm5_rm_rd(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_sbi_imm5_rm_rd(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 	
-	const uint8_t operation = mlBFEXT(opcode, 12, 11);
-	const uint8_t imm5 = mlBFEXT(opcode, 10, 6);
+	const uint8_t operation = mlBFEXT(IR, 12, 11);
+	const uint8_t imm5 = mlBFEXT(IR, 10, 6);
 
-	const csx_reg_t rm = mlBFEXT(opcode, 5, 3);
+	const csx_reg_t rm = mlBFEXT(IR, 5, 3);
 	const uint32_t rm_v = csx_reg_get(core, rm);
 
-	const csx_reg_t rd = mlBFEXT(opcode, 2, 0);
+	const csx_reg_t rd = mlBFEXT(IR, 2, 0);
 	
 	uint8_t shift = imm5;
 	const char *ops = "";
@@ -636,16 +633,16 @@ static void csx_core_thumb_sbi_imm5_rm_rd(csx_core_p core, uint16_t opcode)
 	csx_reg_set(core, rd, rd_v);
 }
 
-static void csx_core_thumb_pop_push(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_pop_push(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 	
 //	struct {
-		const int bit_l = BEXT(opcode, 11);
-		const int bit_r = BEXT(opcode, 8);
+		const int bit_l = BEXT(IR, 11);
+		const int bit_r = BEXT(IR, 8);
 //	}bit;
 	
-	const uint8_t rlist = mlBFEXT(opcode, 7, 0);
+	const uint8_t rlist = mlBFEXT(IR, 7, 0);
 
 	const uint32_t sp_v = SP;
 	
@@ -730,16 +727,16 @@ static void csx_core_thumb_pop_push(csx_core_p core, uint16_t opcode)
 	}
 }
 
-static void csx_core_thumb_sdp_rms_rdn(csx_core_p core, uint16_t opcode)
+static void csx_core_thumb_sdp_rms_rdn(csx_core_p core)
 {
 	CORE_T(const int cce = 1);
 	
-	const uint8_t operation = mlBFEXT(opcode, 9, 8);
+	const uint8_t operation = mlBFEXT(IR, 9, 8);
 
-	const csx_reg_t rm = mlBFEXT(opcode, 6, 3);
+	const csx_reg_t rm = mlBFEXT(IR, 6, 3);
 	const uint32_t rm_v = csx_reg_get(core, rm);
 
-	const csx_reg_t rd = mlBFEXT(opcode, 2, 0) | BMOV(opcode, 7, 3);
+	const csx_reg_t rd = mlBFEXT(IR, 2, 0) | BMOV(IR, 7, 3);
 	const uint32_t rd_v = csx_reg_get(core, rd);
 	
 	uint32_t res = rd_v;
@@ -758,7 +755,7 @@ static void csx_core_thumb_sdp_rms_rdn(csx_core_p core, uint16_t opcode)
 			break;
 		default:
 			LOG("operation = 0x%01x", operation);
-			csx_core_thumb_disasm(core, IP, opcode);
+			csx_core_thumb_disasm(core, IP, IR);
 			LOG_ACTION(exit(1));
 			break;
 	}
@@ -772,107 +769,107 @@ void csx_core_thumb_step(csx_core_p core)
 {
 	CORE_T(core->ccs = "AL");
 
-	const uint32_t ir = csx_reg_pc_fetch_step_thumb(core);
+	IR = csx_reg_pc_fetch_step_thumb(core);
 
 	uint8_t lsb;
 	uint32_t opcode;
 	
 	for(lsb = 8; lsb <= 13; lsb++)
 	{
-		opcode = ir & mlBF(15, lsb);
+		opcode = IR & mlBF(15, lsb);
 
 		switch(opcode)
 		{
 		/* **** */
 			case	0x0000:
-				if(CSX_CORE_THUMB_SBI_IMM5_RM_RD == (ir & CSX_CORE_THUMB_SBI_IMM5_RM_RD_MASK))
-					return(csx_core_thumb_sbi_imm5_rm_rd(core, ir));
+				if(CSX_CORE_THUMB_SBI_IMM5_RM_RD == (IR & CSX_CORE_THUMB_SBI_IMM5_RM_RD_MASK))
+					return(csx_core_thumb_sbi_imm5_rm_rd(core));
 				break;
 			case	0x1800:
-				if(CSX_CORE_THUMB_ADD_SUB_RN_RD == (ir & CSX_CORE_THUMB_ADD_SUB_RN_RD_MASK))
-					return(csx_core_thumb_add_sub_rn_rd(core, ir));
+				if(CSX_CORE_THUMB_ADD_SUB_RN_RD == (IR & CSX_CORE_THUMB_ADD_SUB_RN_RD_MASK))
+					return(csx_core_thumb_add_sub_rn_rd(core));
 				break;
 			case	0x2000:
-				if(CSX_CORE_THUMB_ASCM_RD_I8(0) == (ir & CSX_CORE_THUMB_ASCM_RD_I8_MASK))
-					return(csx_core_thumb_ascm_rd_i(core, ir));
+				if(CSX_CORE_THUMB_ASCM_RD_I8(0) == (IR & CSX_CORE_THUMB_ASCM_RD_I8_MASK))
+					return(csx_core_thumb_ascm_rd_i(core));
 				break;
 			case	0x4000:
-				if(CSX_CORE_THUMB_DP_RMS_RDN == (ir & CSX_CORE_THUMB_DP_RMS_RDN_MASK))
-					return(csx_core_thumb_dp_rms_rdn(core, ir));
+				if(CSX_CORE_THUMB_DP_RMS_RDN == (IR & CSX_CORE_THUMB_DP_RMS_RDN_MASK))
+					return(csx_core_thumb_dp_rms_rdn(core));
 				break;
 			case	0x4400:
-				if(CSX_CORE_THUMB_SDP_RMS_RDN(0) == (ir & CSX_CORE_THUMB_SDP_RMS_RDN_MASK))
-					return(csx_core_thumb_sdp_rms_rdn(core, ir));
+				if(CSX_CORE_THUMB_SDP_RMS_RDN(0) == (IR & CSX_CORE_THUMB_SDP_RMS_RDN_MASK))
+					return(csx_core_thumb_sdp_rms_rdn(core));
 				break;
 			case	0x4700:
-				if(CSX_CORE_THUMB_BX == (ir & CSX_CORE_THUMB_BX_MASK))
-					return(csx_core_thumb_bx(core, ir));
+				if(CSX_CORE_THUMB_BX == (IR & CSX_CORE_THUMB_BX_MASK))
+					return(csx_core_thumb_bx(core));
 				break;
 			case	0x4800:
-				if(CSX_CORE_THUMB_LDST_PC_RD_I == (ir & CSX_CORE_THUMB_LDST_PC_RD_I_MASK))
-					return(csx_core_thumb_ldst_rd_i(core, ir));
+				if(CSX_CORE_THUMB_LDST_PC_RD_I == (IR & CSX_CORE_THUMB_LDST_PC_RD_I_MASK))
+					return(csx_core_thumb_ldst_rd_i(core));
 				break;
 			case	0x5000:
-				if(CSX_CORE_THUMB_LDST_RM_RN_RD == (ir & CSX_CORE_THUMB_LDST_RM_RN_RD_MASK))
-					return(csx_core_thumb_ldst_rm_rn_rd(core, ir));
+				if(CSX_CORE_THUMB_LDST_RM_RN_RD == (IR & CSX_CORE_THUMB_LDST_RM_RN_RD_MASK))
+					return(csx_core_thumb_ldst_rm_rn_rd(core));
 				break;
 			case	0x5600:
 			case	0x5e00:
 				break;
 			case	0x6000:
-				if(CSX_CORE_THUMB_LDST_BW_O_RN_RD == (ir & CSX_CORE_THUMB_LDST_BW_O_RN_RD_MASK))
-					return(csx_core_thumb_ldst_bwh_o_rn_rd(core, ir));
+				if(CSX_CORE_THUMB_LDST_BW_O_RN_RD == (IR & CSX_CORE_THUMB_LDST_BW_O_RN_RD_MASK))
+					return(csx_core_thumb_ldst_bwh_o_rn_rd(core));
 				break;
 			case	0x8000:
-				if(CSX_CORE_THUMB_LDST_H_O_RN_RD == (ir & CSX_CORE_THUMB_LDST_H_O_RN_RD_MASK))
-					return(csx_core_thumb_ldst_bwh_o_rn_rd(core, ir));
+				if(CSX_CORE_THUMB_LDST_H_O_RN_RD == (IR & CSX_CORE_THUMB_LDST_H_O_RN_RD_MASK))
+					return(csx_core_thumb_ldst_bwh_o_rn_rd(core));
 				break;
 			case	0x9000:
-				if(CSX_CORE_THUMB_LDST_SP_RD_I == (ir & CSX_CORE_THUMB_LDST_SP_RD_I_MASK))
-					return(csx_core_thumb_ldst_rd_i(core, ir));
+				if(CSX_CORE_THUMB_LDST_SP_RD_I == (IR & CSX_CORE_THUMB_LDST_SP_RD_I_MASK))
+					return(csx_core_thumb_ldst_rd_i(core));
 				break;
 			case	0xa000:
-				if(CSX_CORE_THUMB_ADD_RD_PCSP_I == (ir & CSX_CORE_THUMB_ADD_RD_PCSP_I_MASK))
-					return(csx_core_thumb_add_rd_pcsp_i(core, ir));
+				if(CSX_CORE_THUMB_ADD_RD_PCSP_I == (IR & CSX_CORE_THUMB_ADD_RD_PCSP_I_MASK))
+					return(csx_core_thumb_add_rd_pcsp_i(core));
 				break;
 			case	0xb000:
-				if(CSX_CORE_THUMB_ADD_SUB_SP_I7 == (ir & CSX_CORE_THUMB_ADD_SUB_SP_I7_MASK))
-					return(csx_core_thumb_add_sub_sp_i7(core, ir));
+				if(CSX_CORE_THUMB_ADD_SUB_SP_I7 == (IR & CSX_CORE_THUMB_ADD_SUB_SP_I7_MASK))
+					return(csx_core_thumb_add_sub_sp_i7(core));
 				break;
 			case	0xb400:
 			case	0xbc00:
-				if(CSX_CORE_THUMB_POP_PUSH(0) == (ir & CSX_CORE_THUMB_POP_PUSH_MASK))
-					return(csx_core_thumb_pop_push(core, ir));
+				if(CSX_CORE_THUMB_POP_PUSH(0) == (IR & CSX_CORE_THUMB_POP_PUSH_MASK))
+					return(csx_core_thumb_pop_push(core));
 				break;
 			case	0xc000:
-				if(CSX_CORE_THUMB_LDSTM_RN_RXX(0) == (ir & CSX_CORE_THUMB_LDSTM_RN_RXX_MASK))
-					return(csx_core_thumb_ldstm_rn_rxx(core, ir));
+				if(CSX_CORE_THUMB_LDSTM_RN_RXX(0) == (IR & CSX_CORE_THUMB_LDSTM_RN_RXX_MASK))
+					return(csx_core_thumb_ldstm_rn_rxx(core));
 				break;
 			case	0xd000:
-				opcode = ir & mlBF(15, 8);
+				opcode = IR & mlBF(15, 8);
 				switch(opcode)
 				{
 					case	0xde00: /* undefined */
 					case	0xdf00: /* swi */
-//						TRACE("ir = 0x%04x, opcode = 0x%04x, lsb", ir, opcode);
+//						TRACE("ir = 0x%04x, opcode = 0x%04x, lsb", IP, opcode);
 						break;
 					default:
-						return(csx_core_thumb_bcc(core, ir));
+						return(csx_core_thumb_bcc(core));
 						break;
 				} break;
 			case	0xe000:
 			case	0xf000:
-					return(csx_core_thumb_bxx(core, ir));
+					return(csx_core_thumb_bxx(core));
 				break;
 			default:
-//				TRACE("ir = 0x%04x, opcode = 0x%04x, lsb = 0x%02x", ir, opcode, lsb);
+//				TRACE("ir = 0x%04x, opcode = 0x%04x, lsb = 0x%02x", IR, opcode, lsb);
 				break;
 		/* **** */
 		}
 	}//while(lsb-- > 8);
 
-	TRACE("ir = 0x%04x, opcode = 0x%04x, lsb = 0x%02x", ir, opcode, lsb);
+	TRACE("ir = 0x%04x, opcode = 0x%04x, lsb = 0x%02x", IR, opcode, lsb);
 
-	csx_core_thumb_disasm(core, PC, ir);
+	csx_core_thumb_disasm(core, IP, IR);
 	LOG_ACTION(exit(1));
 }
