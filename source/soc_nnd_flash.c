680c61384f1624a661a58c9c13569584cd331090
bsekisser <squirmyworms@embarqmail.com>
2022-08-30 17:34:07 -0400

    separate unit data per unit.
    	modified:   source/soc_nnd_flash.c

--- source/soc_nnd_flash.c
+++ source/soc_nnd_flash.c
@@ -17,11 +17,16 @@
 
 /* **** */
 
+typedef struct soc_nnd_unit_t* soc_nnd_unit_p;
+typedef struct soc_nnd_unit_t {
+	uint32_t						cl;
+	uint32_t						status;
+}soc_nnd_unit_t;
+
 typedef struct soc_nnd_t {
 	csx_p							csx;
 
-	uint32_t						cl;
-	uint32_t						status;
+	soc_nnd_unit_t					unit[4];
 }soc_nnd_t;
 
 /* **** */
@@ -31,6 +36,24 @@ enum {
 	CLE = 0x02,
 	ALE = 0x04,
 };
+
+const uint16_t soc_nnd_flash_id = 0x79ec; /* samsung */
+//const uint16_t soc_nnd_flash_id = 0x7998; /* toshiba */
+//const uint16_t soc_nnd_flash_id = 0x7904; /* fujitsu */
+
+/* **** */
+
+static soc_nnd_unit_p _soc_nnd_flash_unit(soc_nnd_p nnd, uint32_t addr)
+{
+	const uint cs = addr >> 26;
+	const soc_nnd_unit_p unit = &nnd->unit[cs];
+
+	LOG("nnd = 0x%08x, addr = 0x%08x, cs = 0x%02x, unit = 0x%08x", (uint)nnd, addr, cs, (uint)unit);
+
+	assert(cs < 4);
+
+	return(unit);
+}
 
 /* **** */
 
@@ -48,61 +71,56 @@ int soc_nnd_flash_init(csx_p csx, soc_nnd_h h2nnd)
 	return(0);
 }
 
-const uint16_t soc_nnd_flash_id = 0x79ec; /* samsung */
-//const uint16_t soc_nnd_flash_id = 0x7998; /* toshiba */
-//const uint16_t soc_nnd_flash_id = 0x7904; /* fujitsu */
-
 uint32_t soc_nnd_flash_read(soc_nnd_p nnd, uint32_t addr, uint size)
 {
-	const uint unit = addr >> 26;
-	
-	assert(unit < 4);
+	const soc_nnd_unit_p unit = _soc_nnd_flash_unit(nnd, addr);
 
-	uint index = (nnd->cl & 0x0f);
+	uint index = (unit->cl & 0x0f);
 
 	uint value = 0;
 
 	if(RWD == (addr & 0x7)) {
-		if(0x70 == (nnd->cl & 0xff)) { /* read status */
-			value = nnd->status;
-		} else if(0x90 == (nnd->cl & 0xf0)) { /* read id */
+		if(0x70 == (unit->cl & 0xff)) { /* read status */
+			value = unit->status;
+		} else if(0x90 == (unit->cl & 0xf0)) { /* read id */
 			value = (soc_nnd_flash_id >> (index << 3)) & 0xff;
-			nnd->cl = (nnd->cl & 0xf0) | (index + 1);
+			unit->cl = (unit->cl & 0xf0) | (index + 1);
 		}
 	}
-	
-	LOG("addr = 0x%08x, unit = 0x%08x, value = 0x%08x, size = 0x%08x, cl = 0x%08x", addr, unit, value, size, nnd->cl);
+
+	LOG("addr = 0x%08x, value = 0x%08x, size = 0x%08x, cl = 0x%08x", addr, value, size, unit->cl);
 
 	return(value);
 }
 
-static void soc_nnd_flash_write_cle(soc_nnd_p nnd, uint unit, uint32_t value, uint size)
+static void soc_nnd_flash_write_cle(soc_nnd_p nnd, soc_nnd_unit_p unit, uint32_t value, uint size)
 {
-	LOG("unit = 0x%08x, value = 0x%08x, size = 0x%08x, cl = 0x%08x", unit, value, size, nnd->cl);
-	
+	LOG("value = 0x%08x, size = 0x%08x, cl = 0x%08x", value, size, unit->cl);
+
 	if(0xff == value) { /* reset */
-		nnd->cl = 0;
-		nnd->status = 0;
-		BSET(nnd->status, 7); /* not write protected */
-		BSET(nnd->status, 6); /* device ready */
+		unit->cl = 0;
+		unit->status = 0;
+		BSET(unit->status, 7); /* not write protected */
+		BSET(unit->status, 6); /* device ready */
 	} else {
-		nnd->cl <<= 8;
-		nnd->cl |= (value & 0xff);
+		unit->cl <<= 8;
+		unit->cl |= (value & 0xff);
 	}
 
-	LOG("unit = 0x%08x, value = 0x%08x, size = 0x%08x, cl = 0x%08x", unit, value, size, nnd->cl);
+	LOG("value = 0x%08x, size = 0x%08x, cl = 0x%08x", value, size, unit->cl);
 }
 
 void soc_nnd_flash_write(soc_nnd_p nnd, uint32_t addr, uint32_t value, uint size)
 {
-	const uint unit = addr >> 26;
-	
-	assert(unit < 4);
-	
+	const soc_nnd_unit_p unit = _soc_nnd_flash_unit(nnd, addr);
+
 	const uint32_t offset = addr & 0x07;
-	
+
+	LOG("addr = 0x%08x, value = 0x%08x, size = 0x%08x, cl = 0x%08x", addr, value, size, unit->cl);
+
 	switch(offset) {
 		case CLE:
+			LOG("CLE: value = 0x%08x, size = 0x%08x", value, size);
 			soc_nnd_flash_write_cle(nnd, unit, value, size);
 			break;
 	}
