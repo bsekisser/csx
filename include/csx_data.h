e000f9f941729306de47b1c25e60e303731f2f10
bsekisser <squirmyworms@embarqmail.com>
2023-01-03 09:05:20 -0500

    common read write data size access in header
    	modified:   include/csx_data.h
    	modified:   source/csx_data.c

--- include/csx_data.h
+++ include/csx_data.h
@@ -26,10 +26,40 @@ typedef struct csx_data_bit_t {
 
 void csx_data_bit_bmas(void* p2data, csx_data_bit_p sdbp, uint set);
 uint csx_data_bit_read(void* p2src, csx_data_bit_p sdbp);
-uint32_t csx_data_read(void* p2src, uint8_t size);
-void csx_data_write(void* p2dst, uint32_t value, uint8_t size);
+
+uint32_t csx_data_read_x(void* p2src, uint8_t size);
+void csx_data_write_x(void* p2dst, uint32_t value, uint8_t size);
 
 /* **** */
+
+static inline uint32_t csx_data_read(void* p2src, uint8_t size) {
+	switch(size) {
+		case 4:
+			return(le32toh(*(uint32_t*)p2src));
+		case 2:
+			return(le16toh(*(uint16_t*)p2src));
+		case 1:
+			return(*(uint8_t*)p2src);
+	}
+
+	return(csx_data_read_x(p2src, size));
+}
+
+static inline void csx_data_write(void* p2dst, uint32_t value, uint8_t size) {
+	switch(size) {
+		case 4:
+			*(uint32_t*)p2dst = htole32((uint32_t)value);
+			return;
+		case 2:
+			*(uint16_t*)p2dst = htole16((uint16_t)value);
+			return;
+		case 1:
+			*(uint8_t*)p2dst = (uint8_t)value;
+			return;
+	}
+
+	csx_data_write_x(p2dst, value, size);
+}
 
 static inline void csx_data_bit_clear(void* p2data, csx_data_bit_p sdbp) {
 	csx_data_bit_bmas(p2data, sdbp, 0);
--- source/csx_data.c
+++ source/csx_data.c
@@ -19,13 +19,13 @@ void csx_data_bit_bmas(void* p2dst, csx_data_bit_p sdbp, uint set)
 uint csx_data_bit_read(void* p2src, csx_data_bit_p sdbp)
 {
 	const void* p2data = p2src + sdbp->offset;
-	
+
 	const uint value = csx_data_read((void*)p2data, sdbp->size);
 
 	return(BEXT(value, sdbp->bit));
 }
 
-uint32_t csx_data_read(void* p2src, uint8_t size)
+uint32_t csx_data_read_x(void* p2src, uint8_t size)
 {
 	uint32_t res = 0;
 
@@ -37,7 +37,7 @@ uint32_t csx_data_read(void* p2src, uint8_t size)
 	return(res);
 }
 
-void csx_data_write(void* p2dst, uint32_t value, uint8_t size)
+void csx_data_write_x(void* p2dst, uint32_t value, uint8_t size)
 {
 	uint8_t* dst = (uint8_t*)p2dst;
 
