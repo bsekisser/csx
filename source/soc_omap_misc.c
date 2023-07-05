f3afb0b5908961e54afa346de4de1be4f5ee5263
bsekisser <squirmyworms@embarqmail.com>
2023-07-05 11:58:15 -0400

    _soc_omap_misc_fe_60_mem_access, _soc_omap_misc_fe_78_mem_access
    _soc_omap_misc_mem_access --> _soc_omap_misc_fe_60_mem_access
    data --> x_fe_60
    
    	modified:   source/soc_omap_misc.c

--- source/soc_omap_misc.c
+++ source/soc_omap_misc.c
@@ -26,8 +26,9 @@ typedef struct soc_omap_misc_t {
 	csx_p csx;
 	csx_mmio_p mmio;
 	
-	uint8_t data[0x100];
 	uint8_t sossi[0x100];
+	uint8_t x_fe_60[0x100];
+	uint8_t x_fe_78[0x100];
 
 	callback_qlist_elem_t atexit;
 }soc_omap_misc_t;
@@ -45,13 +46,13 @@ int __soc_omap_misc_atexit(void* param)
 
 /* **** */
 
-static uint32_t _soc_omap_misc_mem_access(void* param, uint32_t ppa, size_t size, uint32_t* write)
+static uint32_t _soc_omap_misc_fe_60_mem_access(void* param, uint32_t ppa, size_t size, uint32_t* write)
 {
 	const soc_omap_misc_p misc = param;
 	const csx_p csx = misc->csx;
 	
 	csx_data_target_t target = {
-		.base = misc->data,
+		.base = misc->x_fe_60,
 		.offset = ppa & 0xff,
 		.size = sizeof(uint32_t),
 	};
@@ -70,6 +71,25 @@ static uint32_t _soc_omap_misc_mem_access(void* param, uint32_t ppa, size_t size
 	return(data);
 }
 
+static uint32_t _soc_omap_misc_fe_78_mem_access(void* param, uint32_t ppa, size_t size, uint32_t* write)
+{
+	const soc_omap_misc_p misc = param;
+	const csx_p csx = misc->csx;
+	
+	csx_data_target_t target = {
+		.base = misc->x_fe_78,
+		.offset = ppa & 0xff,
+		.size = sizeof(uint32_t),
+	};
+	
+	uint32_t data = csx_data_target_mem_access(&target, size, write);
+
+	if(_trace_mmio_misc)
+		CSX_MMIO_TRACE_MEM_ACCESS(csx, ppa, size, write, data);
+
+	return(data);
+}
+
 static uint32_t _soc_omap_misc_sossi_mem_access(void* param, uint32_t ppa, size_t size, uint32_t* write)
 {
 	const soc_omap_misc_p misc = param;
@@ -78,7 +98,7 @@ static uint32_t _soc_omap_misc_sossi_mem_access(void* param, uint32_t ppa, size_
 	csx_data_target_t target = {
 		.base = misc->sossi,
 		.offset = ppa & 0xff,
-		.size = sizeof(uint32_t),
+		.size = sizeof(uint16_t),
 	};
 	
 	uint32_t data = csx_data_target_mem_access(&target, size, write);
@@ -93,22 +113,38 @@ static uint32_t _soc_omap_misc_sossi_mem_access(void* param, uint32_t ppa, size_
 
 static csx_mmio_access_list_t __soc_omap_misc_acl[] = {
 	MMIO_TRACE_FN(0xfffb, 0xac00, 0x0000, 0x0000, xfffb_ac00, _soc_omap_misc_sossi_mem_access)
-	MMIO_TRACE_FN(0xfffb, 0xac04, 0x0000, 0x0000, xfffb_ac00, _soc_omap_misc_sossi_mem_access)
-	MMIO_TRACE_FN(0xfffb, 0xac08, 0x0000, 0x0000, xfffb_ac00, _soc_omap_misc_sossi_mem_access)
-	MMIO_TRACE_FN(0xfffb, 0xac0c, 0x0000, 0x0000, xfffb_ac00, _soc_omap_misc_sossi_mem_access)
-	MMIO_TRACE_FN(0xfffb, 0xac10, 0x0000, 0x0000, xfffb_ac00, _soc_omap_misc_sossi_mem_access)
-	MMIO_TRACE_FN(0xfffb, 0xac14, 0x0000, 0x0000, xfffb_ac00, _soc_omap_misc_sossi_mem_access)
-	MMIO_TRACE_FN(0xfffb, 0xac18, 0x0000, 0x0000, xfffb_ac00, _soc_omap_misc_sossi_mem_access)
-	MMIO_TRACE_FN(0xfffb, 0xac1c, 0x0000, 0x0000, xfffb_ac00, _soc_omap_misc_sossi_mem_access)
-	MMIO_TRACE_FN(0xfffb, 0xac20, 0x0000, 0x0000, xfffb_ac00, _soc_omap_misc_sossi_mem_access)
+	MMIO_TRACE_FN(0xfffb, 0xac04, 0x0000, 0x0000, xfffb_ac04, _soc_omap_misc_sossi_mem_access)
+	MMIO_TRACE_FN(0xfffb, 0xac08, 0x0000, 0x0000, xfffb_ac08, _soc_omap_misc_sossi_mem_access)
+	MMIO_TRACE_FN(0xfffb, 0xac0c, 0x0000, 0x0000, xfffb_ac0c, _soc_omap_misc_sossi_mem_access)
+	MMIO_TRACE_FN(0xfffb, 0xac10, 0x0000, 0x0000, xfffb_ac10, _soc_omap_misc_sossi_mem_access)
+	MMIO_TRACE_FN(0xfffb, 0xac14, 0x0000, 0x0000, xfffb_ac14, _soc_omap_misc_sossi_mem_access)
+	MMIO_TRACE_FN(0xfffb, 0xac18, 0x0000, 0x0000, xfffb_ac18, _soc_omap_misc_sossi_mem_access)
+	MMIO_TRACE_FN(0xfffb, 0xac1c, 0x0000, 0x0000, xfffb_ac1c, _soc_omap_misc_sossi_mem_access)
+	MMIO_TRACE_FN(0xfffb, 0xac20, 0x0000, 0x0000, xfffb_ac20, _soc_omap_misc_sossi_mem_access)
 //
-	MMIO_TRACE_FN(0xfffe, 0x6010, 0x0000, 0x0000, xfffe_6010, _soc_omap_misc_mem_access)
-	MMIO_TRACE_FN(0xfffe, 0x6014, 0x0000, 0x0000, xfffe_6014, _soc_omap_misc_mem_access)
-	MMIO_TRACE_FN(0xfffe, 0x6018, 0x0000, 0x0000, xfffe_6018, _soc_omap_misc_mem_access)
-	MMIO_TRACE_FN(0xfffe, 0x601c, 0x0000, 0x0000, xfffe_601c, _soc_omap_misc_mem_access)
-	MMIO_TRACE_FN(0xfffe, 0x6020, 0x0000, 0x0000, xfffe_6020, _soc_omap_misc_mem_access)
-	MMIO_TRACE_FN(0xfffe, 0x6030, 0x0000, 0x0000, xfffe_6030, _soc_omap_misc_mem_access)
-	MMIO_TRACE_FN(0xfffe, 0x6034, 0x0000, 0x0000, xfffe_6043, _soc_omap_misc_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x6010, 0x0000, 0x0000, xfffe_6010, _soc_omap_misc_fe_60_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x6014, 0x0000, 0x0000, xfffe_6014, _soc_omap_misc_fe_60_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x6018, 0x0000, 0x0000, xfffe_6018, _soc_omap_misc_fe_60_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x601c, 0x0000, 0x0000, xfffe_601c, _soc_omap_misc_fe_60_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x6020, 0x0000, 0x0000, xfffe_6020, _soc_omap_misc_fe_60_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x6030, 0x0000, 0x0000, xfffe_6030, _soc_omap_misc_fe_60_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x6034, 0x0000, 0x0000, xfffe_6043, _soc_omap_misc_fe_60_mem_access)
+//
+	MMIO_TRACE_FN(0xfffe, 0x7800, 0x0000, 0x0000, xfffe_7800, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x7802, 0x0000, 0x0000, xfffe_7802, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x7804, 0x0000, 0x0000, xfffe_7804, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x7806, 0x0000, 0x0000, xfffe_7806, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x7808, 0x0000, 0x0000, xfffe_7808, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x780a, 0x0000, 0x0000, xfffe_780a, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x780c, 0x0000, 0x0000, xfffe_780c, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x780e, 0x0000, 0x0000, xfffe_780e, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x7810, 0x0000, 0x0000, xfffe_7810, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x7812, 0x0000, 0x0000, xfffe_7812, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x7814, 0x0000, 0x0000, xfffe_7814, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x7816, 0x0000, 0x0000, xfffe_7816, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x7818, 0x0000, 0x0000, xfffe_7818, _soc_omap_misc_fe_78_mem_access)
+	MMIO_TRACE_FN(0xfffe, 0x781a, 0x0000, 0x0000, xfffe_781a, _soc_omap_misc_fe_78_mem_access)
+
 	{ .ppa = ~0U, },
 };
 
