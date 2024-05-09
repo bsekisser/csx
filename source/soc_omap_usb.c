2cb05bf80ab67bf51acf8b589f0f12558f7b2a1d
bsekisser <squirmyworms@embarqmail.com>
2024-05-09 00:37:04 -0400

    usb_otg_syscon1
    	modified:   source/soc_omap_usb.c

--- source/soc_omap_usb.c
+++ source/soc_omap_usb.c
@@ -12,6 +12,7 @@
 #include "libbse/include/err_test.h"
 #include "libbse/include/handle.h"
 #include "libbse/include/log.h"
+#include "libbse/include/mem_access.h"
 #include "libbse/include/unused.h"
 
 /* **** */
@@ -25,11 +26,20 @@ typedef struct soc_omap_usb_t {
 	csx_p csx;
 	csx_mmio_p mmio;
 
+	struct {
+		uint32_t syscon1;
+	}otg;
+
 	uint8_t data[0x100];
 
 	callback_qlist_elem_t atexit;
 	callback_qlist_elem_t atreset;
 }soc_omap_usb_t;
+
+/* **** */
+
+//static void ___set_reset_done(soc_omap_usb_p const usb, const int set)
+//{ BMAS(usb>syscon_1, 2, set); }
 
 /* **** */
 
@@ -50,6 +60,7 @@ static int __soc_omap_usb_atreset(void* param)
 	}
 
 //	soc_omap_usb_p usb = param;
+//	___set_reset_done(param, 1);
 
 	return(0);
 	UNUSED(param);
@@ -61,7 +72,7 @@ static uint32_t _soc_omap_usb_client_mem_access(void* param, uint32_t ppa, size_
 {
 	if(_check_pedantic_mmio_size)
 		assert(sizeof(uint16_t) == size);
-	
+
 	const soc_omap_usb_p usb = param;
 	const csx_p csx = usb->csx;
 
@@ -75,9 +86,34 @@ static uint32_t _soc_omap_usb_client_mem_access(void* param, uint32_t ppa, size_
 	return(data);
 }
 
+static uint32_t _soc_omap_usb_otg_mem_access(void* param, uint32_t ppa, size_t size, uint32_t* write)
+{
+	if(_check_pedantic_mmio_size)
+		assert(sizeof(uint32_t) == size);
+
+	const soc_omap_usb_p usb = param;
+	const csx_p csx = usb->csx;
+
+	uint32_t data = 0xdeadbeef;
+	const uint8_t offset = ppa & 0xff;
+
+	switch(offset) {
+		case 4: // usb_otg_syscon1
+			data = mem_32_access(&usb->otg.syscon1, write) | _BV(2);
+			break;
+	}
+
+	if(_trace_mmio_usb_client)
+		CSX_MMIO_TRACE_MEM_ACCESS(csx, ppa, size, write, data);
+
+	return(data);
+}
+
+
 /* **** */
 
 static csx_mmio_access_list_t _soc_omap_usb_client_acl[] = {
+	MMIO_TRACE_FN(0xfffb, 0x0404, 0x0000, 0x0000, USB_OTG_SYSCON1, _soc_omap_usb_otg_mem_access)
 	MMIO_TRACE_FN(0xfffb, 0x4018, 0x0000, 0x0000, USB_CLNT_SYSCON1, _soc_omap_usb_client_mem_access)
 	{ .ppa = ~0U, },
 };
@@ -112,7 +148,7 @@ soc_omap_usb_p soc_omap_usb_alloc(csx_p csx, csx_mmio_p mmio, soc_omap_usb_h h2u
 void soc_omap_usb_init(soc_omap_usb_p usb)
 {
 	ERR_NULL(usb);
-	
+
 	if(_trace_init) {
 		LOG();
 	}
